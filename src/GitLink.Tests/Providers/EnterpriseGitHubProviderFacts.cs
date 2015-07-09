f209ba31b56a1620293e3fb0513e422cb980b546
shewitt <shewitt@fanatics.com>
2015-07-09 16:48:16 -0400

    Added configurable EnterpriseGitHub provider. Added unit tests to support new provider.

---
+++ src/GitLink.Tests/Providers/EnterpriseGitHubProviderFacts.cs
@@ -0,0 +1,118 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GitHubProviderFacts.cs" company="CatenaLogic">
+//   Copyright (c) 2014 - 2014 CatenaLogic. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace GitLink.Tests.Providers
+{
+    using GitLink.Providers;
+    using NUnit.Framework;
+    using System.Configuration;
+
+    public class EnterpriseGitHubProviderFacts
+    {
+        [TestFixture]
+        public class TheEnterpriseGitHubProviderInitialization
+        {
+            [TestCase]
+            public void ReturnsValidInitialization()
+            {
+                var provider = new EnterpriseGitHubProvider();
+                var valid = provider.Initialize("https://catenalogic-github.catenalogic.corp/CatenaLogic/GitLink");
+
+                Assert.IsTrue(valid);
+            }
+
+            [TestCase]
+            public void ReturnsInValidInitialization()
+            {
+                var provider = new EnterpriseGitHubProvider();
+                var valid = provider.Initialize("https://bitbucket.org/CatenaLogic/GitLink");
+
+                Assert.IsFalse(valid);
+
+                valid = provider.Initialize("https://github.com/CatenaLogic/GitLink");
+
+                Assert.IsFalse(valid);
+            }
+
+            [TestCase]
+            public void ReturnsValidNoMatchInitialization()
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                var resetMethod = typeof(System.Collections.Specialized.NameObjectCollectionBase).GetMethod("Reset", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance, null, new System.Type[] { }, null);
+                resetMethod.Invoke(appSettings, null);
+
+                var provider = new EnterpriseGitHubProvider();
+                var valid = provider.Initialize("https://bitbucket.org/CatenaLogic/GitLink");
+
+                Assert.IsFalse(valid);
+
+                valid = provider.Initialize("https://github.com/CatenaLogic/GitLink");
+
+                Assert.IsFalse(valid);
+
+                valid = provider.Initialize("https://catenalogic-github.catenalogic.corp/CatenaLogic/GitLink");
+
+                Assert.IsFalse(valid);
+
+                valid = provider.Initialize("https://catenalogic-github.catenalogic.corp/CatenaLogic/GitLink");
+
+                Assert.IsTrue(valid);
+
+                ConfigurationManager.RefreshSection("appSettings");
+            }
+        }
+
+        [TestFixture]
+        public class TheEnterpriseGitHubProviderProperties
+        {
+            [TestCase]
+            public void ReturnsValidCompany()
+            {
+                var provider = new EnterpriseGitHubProvider();
+                provider.Initialize("https://catenalogic-github.catenalogic.corp/CatenaLogic/GitLink");
+
+                Assert.AreEqual("CatenaLogic", provider.CompanyName);
+            }
+
+            [TestCase]
+            public void ReturnsValidCompanyUrl()
+            {
+                var provider = new EnterpriseGitHubProvider();
+                provider.Initialize("https://catenalogic-github.catenalogic.corp/CatenaLogic/GitLink");
+
+                Assert.AreEqual("https://catenalogic-github.catenalogic.corp/CatenaLogic", provider.CompanyUrl);
+            }
+
+            [TestCase]
+            public void ReturnsValidProject()
+            {
+                var provider = new EnterpriseGitHubProvider();
+                provider.Initialize("https://catenalogic-github.catenalogic.corp/CatenaLogic/GitLink");
+
+                Assert.AreEqual("GitLink", provider.ProjectName);
+            }
+
+            [TestCase]
+            public void ReturnsValidProjectUrl()
+            {
+                var provider = new EnterpriseGitHubProvider();
+                provider.Initialize("https://catenalogic-github.catenalogic.corp/CatenaLogic/GitLink");
+
+                Assert.AreEqual("https://catenalogic-github.catenalogic.corp/CatenaLogic/GitLink", provider.ProjectUrl);
+            }
+
+            [TestCase]
+            public void ReturnsValidRawGitUrl()
+            {
+                var provider = new EnterpriseGitHubProvider();
+                provider.Initialize("https://catenalogic-github.catenalogic.corp/CatenaLogic/GitLink");
+
+                Assert.AreEqual("https://catenalogic-github.catenalogic.corp/CatenaLogic/GitLink/raw", provider.RawGitUrl);
+            }
+        }
+    }
+}
---
+++ src/GitLink/Providers/EnterpriseGitHubProvider.cs
@@ -0,0 +1,79 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EnterpriseGitHubProvider.cs" company="CatenaLogic">
+//   Copyright (c) 2014 - 2014 CatenaLogic. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace GitLink.Providers
+{
+    using System;
+    using System.Text.RegularExpressions;
+    using GitTools.Git;
+    using System.Configuration;
+    using Catel.Logging;
+
+    public class EnterpriseGitHubProvider : ProviderBase
+    {
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+
+        private readonly Regex _gitHubRegex;
+        private readonly string _rawGitUrlFormat;
+
+        /// <summary>
+        /// Loads GitHubRegex and RawGitUrlFormat from EnterpriseGitHub.config.
+        /// </summary>
+        public EnterpriseGitHubProvider()
+            : base(new GitPreparer())
+        {
+            try
+            {
+                var regex = ConfigurationManager.AppSettings["gitLink.enterpriseGitHub.gitHubUrlRegex"];
+                var format = ConfigurationManager.AppSettings["gitLink.enterpriseGitHub.rawGitUrlFormat"];
+
+                _gitHubRegex = new Regex(regex);
+
+                _rawGitUrlFormat = format;
+            }
+            catch(Exception ex)
+            {
+                Log.Info(ex, "EnterpriseGitHubProvider not loaded.");
+                _gitHubRegex = new Regex("enterprise github provider do not match");
+                _rawGitUrlFormat = "{0}/{1}";
+            }
+        }
+
+        public override string RawGitUrl
+        {
+            get { return String.Format(_rawGitUrlFormat, CompanyName, ProjectName); }
+        }
+
+        public override bool Initialize(string url)
+        {
+            var match = _gitHubRegex.Match(url);
+
+            if (!match.Success)
+            {
+                return false;
+            }
+
+            CompanyName = match.Groups["company"].Value;
+            CompanyUrl = match.Groups["companyurl"].Value;
+
+            ProjectName = match.Groups["project"].Value;
+            ProjectUrl = match.Groups["url"].Value;
+
+            if (!CompanyUrl.StartsWith("https://", StringComparison.InvariantCultureIgnoreCase))
+            {
+                CompanyUrl = String.Concat("https://", CompanyUrl);
+            }
+
+            if (!ProjectUrl.StartsWith("https://", StringComparison.InvariantCultureIgnoreCase))
+            {
+                ProjectUrl = String.Concat("https://", ProjectUrl);
+            }
+
+            return true;
+        }
+    }
+}
